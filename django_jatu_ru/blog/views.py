47eff5ec0e1ba3f6790185cbe48201c6a531a7e4
Alfir Tyakin <alfir777@rambler.ru>
2021-08-27 16:50:57 +0500

    add views and templates

--- django_jatu_ru/blog/views.py
+++ django_jatu_ru/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Blog
+
+
+def index(request):
+    blogs = Blog.objects.order_by('-created_at')
+    context = {
+        'blogs': blogs,
+        'title': 'Мои блоги'
+    }
+    return render(request, template_name='blog/index.html', context=context)
