5115f5d21d9851c78a1aa2acceb9c70dbefe65aa
Alfir Tyakin <alfir777@rambler.ru>
2021-08-30 14:11:09 +0500

    add custom template tags

--- django_jatu_ru/blog/views.py
+++ django_jatu_ru/blog/views.py
@@ -5,17 +5,14 @@ from .models import Blog, Category
 
 def index(request):
     blogs = Blog.objects.all()
-    categories = Category.objects.all()
     context = {
         'blogs': blogs,
         'title': 'Мои блоги',
-        'categories': categories,
     }
     return render(request, template_name='blog/index.html', context=context)
 
 
 def get_category(request, category_id):
     blogs = Blog.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'blog/category.html', {'blogs': blogs, 'categories': categories, 'category': category})
+    return render(request, 'blog/category.html', {'blogs': blogs, 'category': category})
