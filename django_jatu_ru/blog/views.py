73cc0f50a29d587b84255c6b8912ebd18aa2e90c
Alfir Tyakin <alfir777@rambler.ru>
2021-08-30 11:00:46 +0500

    add parameters to url requests

--- django_jatu_ru/blog/views.py
+++ django_jatu_ru/blog/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 
-from .models import Blog
+from .models import Blog, Category
 
 
 def index(request):
-    blogs = Blog.objects.all
+    blogs = Blog.objects.all()
+    categories = Category.objects.all()
     context = {
         'blogs': blogs,
-        'title': 'Мои блоги'
+        'title': 'Мои блоги',
+        'categories': categories,
     }
     return render(request, template_name='blog/index.html', context=context)
+
+
+def get_category(request, category_id):
+    blogs = Blog.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'blog/category.html', {'blogs': blogs, 'categories': categories, 'category': category})
