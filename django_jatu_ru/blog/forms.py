ecf6b376ebe62227f728bd97476a418469eb710a
Alfir Tyakin <alfir777@rambler.ru>
2021-08-30 18:11:36 +0500

    Add custom validator

--- django_jatu_ru/blog/forms.py
+++ django_jatu_ru/blog/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+import re
 
 from .models import Blog
 
@@ -13,3 +15,9 @@ class BlogForm(forms.ModelForm):
             'content' : forms.Textarea(attrs={"class": "form-control", "rows": 10}),
             'category' : forms.Select(attrs={"class": "form-control"}),
         }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title
