039da5cc7aae205a5cd9845608ee2a7196f48569
Alfir Tyakin <alfir777@rambler.ru>
2022-04-06 12:39:28 +0500

    add python-dotenv

--- django_jatu_ru/django_jatu_ru/settings.py
+++ django_jatu_ru/django_jatu_ru/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+if 'DEBUG' not in os.environ:
+    if os.path.isfile('../.env'):
+        load_dotenv('../.env')
+    else:
+        exit('DO cp ../.env_template.py ../.env and set token!')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,10 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9128i+^0_y42gec1fjckoepkckscbj-d@1r7zpfp55ezgikdvu'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ['DEBUG'] == 'True':
+    DEBUG = True
+elif os.environ['DEBUG'] == 'False':
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 
