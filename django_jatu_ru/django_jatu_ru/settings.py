cc3a8eb3802f15933af39d84a7bd516f38e3b34d
Alfir Tyakin <alfir777@rambler.ru>
2022-04-06 12:13:29 +0500

    update setting

--- django_jatu_ru/django_jatu_ru/settings.py
+++ django_jatu_ru/django_jatu_ru/settings.py
@@ -202,9 +202,9 @@ CKEDITOR_CONFIGS = {
     }
 }
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': os.path.join(BASE_DIR, 'django_cache'),
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+#         'LOCATION': os.path.join(BASE_DIR, 'django_cache'),
+#     }
+# }
